54aca260a6abc1601f8efe434537ee6e80d38f1c
cursecan <anderi.setiawan@gmail.com>
2021-11-23 11:25:20 +0700

    restore

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,8 +12,7 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' },
-      { 'http-equiv': 'Content-Security-Policy', content: 'upgrade-insecure-requests' }
+      { name: 'format-detection', content: 'telephone=no' }
 
     ],
     link: [
